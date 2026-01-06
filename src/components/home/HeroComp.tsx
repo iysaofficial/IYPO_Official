626941506e18ba5a4946cc1bbce506985f0aff20
Eki Iman <ekiimanjordiansyah212@gmail.com>
2026-01-06 10:50:22 +0700

    close regist

--- src/components/home/HeroComp.tsx
+++ src/components/home/HeroComp.tsx
@@ -13,7 +13,8 @@ const HeroComp = () => {
                 </p>
                 <br />
                 <br />
-                <Link href="/registration" className="registration-button">Registration</Link>
+                <button className="registration-button" disabled>Registration Closed</button>
+                {/* <Link href="/registration" className="registration-button">Registration</Link> */}
                 
                 <div className="hero-social-icon">
                     <br />
