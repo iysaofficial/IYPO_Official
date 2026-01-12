7673acfc783e2ecdc46e4fc46001109020b95532
Eki Iman <ekiimanjordiansyah212@gmail.com>
2026-01-12 10:30:38 +0700

    close regist

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,23 +1,23 @@
-import { NextResponse } from 'next/server'
-import type { NextRequest } from 'next/server'
- 
-export function middleware(request: NextRequest) {
-  
-}
- 
-export const config = {
-  matcher: '/registration/:path*',
-}
-
 // import { NextResponse } from 'next/server'
 // import type { NextRequest } from 'next/server'
  
 // export function middleware(request: NextRequest) {
-//   if (request.nextUrl.pathname.startsWith('/registration')) {
-//     return NextResponse.redirect(new URL('/', request.url))
-//   }
+  
 // }
  
 // export const config = {
 //   matcher: '/registration/:path*',
 // }
+
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
+ 
+export function middleware(request: NextRequest) {
+  if (request.nextUrl.pathname.startsWith('/registration')) {
+    return NextResponse.redirect(new URL('/', request.url))
+  }
+}
+ 
+export const config = {
+  matcher: '/registration/:path*',
+}
